493ce585efe1f4ebac1bc2475f70a43606a0a03d
rodrigosoldi <rodrigosoldi@icloud.com>
2016-09-06 13:21:25 -0300

    First Commit

---
+++ XCTestsExample/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  XCTestsExample
+//
+//  Created by Rodrigo Soldi Lopes on 06/09/16.
+//  Copyright © 2016 Rodrigo Soldi Lopes. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
